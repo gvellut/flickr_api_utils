0a303ca1f78c1676569efe3651939b57a0273e45
Guilhem Vellut <g@vellut.com>
2021-11-02 17:11:11 +0100

    Update

--- removetag.py
+++ removetag.py
@@ -7,20 +7,20 @@ flickr = auth_flickr()
 search_result = Addict(
     flickr.photos.search(
         user_id="me",
-        tags="aire-la-ville,satigny",
+        tags="andonnee",
         tag_mode="all",
-        min_uploaded_date="2020-12-19 00:00:00",
-        per_page=100,
+        min_uploaded_date="2021-10-25 00:00:00",
+        per_page=500,
         extras="tags,machine_tags",
     )
 )
 
 # TODO replace wiht walk https://stuvel.eu/flickrapi-doc/7-util.html
 for photo in search_result.photos.photo:
-    if "pont" not in photo.tags:
-        info = Addict(flickr.photos.getInfo(photo_id=photo.id))
-        for tag in info.photo.tags.tag:
-            if tag["raw"] == "haute-savoie":
-                tag_id_to_remove = tag.id
-                resp = flickr.photos.removeTag(tag_id=tag_id_to_remove)
-                break
+    info = Addict(flickr.photos.getInfo(photo_id=photo.id))
+    for tag in info.photo.tags.tag:
+        if tag["raw"] == "andonnee":
+            tag_id_to_remove = tag.id
+            resp = flickr.photos.removeTag(tag_id=tag_id_to_remove)
+            # flickr.photos.addTags(photo_id=photo.id, tags="randonnee")
+            break
